dd73e7b4a9e317948dedea3b3906443848a47f70
Xilin Chen <bismuthchan11@gmail.com>
2023-10-30 09:15:24 +0000

    finished read function

--- Assignment1/utilities.py
+++ Assignment1/utilities.py
@@ -1,7 +1,17 @@
-import pandas as pd
-def read_rail_network(filepath):
-    data = pd.read_csv(filepath)
+import csv
 
+def read_rail_network(filepath):
+    all_stations = []
+    with open(filepath, "r", encoding = "utf-8") as f:
+        read_file = csv.reader(f)
+        col_list = next(read_file)
+        csv_reader = csv.DictReader(f, fieldnames = col_list)
+        for i in csv_reader:
+            st = {}
+            for k, v in i.items():
+                st[k] = v
+            all_stations.append(st)
+    return all_stations
 
     raise NotImplementedError
 
