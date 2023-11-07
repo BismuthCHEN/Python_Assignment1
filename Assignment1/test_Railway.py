95a73e627e5b68e575c103121b3d3b34395fddcb
Xilin Chen <bismuthchan11@gmail.com>
2023-11-07 22:45:52 +0000

    finished testing distance_to and unique crs

--- Assignment1/test_Railway.py
+++ Assignment1/test_Railway.py
@@ -1,6 +1,11 @@
 import pytest
 from railway import Station, RailNetwork
 from utilities import read_rail_network
+from pathlib import Path
+
+# create test set for RailNetwork`s test
+network_csv = Path("uk_stations.csv")
+rail_network = read_rail_network(network_csv)
 
 
 @pytest.mark.parametrize("name, region, crs, lati, longi, hub, expected, errorType", [
@@ -24,10 +29,43 @@ from utilities import read_rail_network
     # test crs
     ("Edinburgh Park", "Scotland", "EDPG", 55.927615, -3.307829, False, "Value Error: crs should be 3-character string with upper letters", ValueError),
     ("Edinburgh Park", "Scotland", "edp", 55.927615, -3.307829, False, "Value Error: crs should be 3-character string with upper letters", ValueError)])
-#["Edinburgh Park", "Scotland", "EDP", 55.927615, -3.307829, False]
-#name, region, crs, lati, longi, hub
 def test_StationTypeError(name, region, crs, lati, longi, hub, expected, errorType):
     with pytest.raises(errorType) as e:
         Station(name, region, crs, lati, longi, hub)
     exec_msg = e.value.args[0]
     assert exec_msg == expected
+
+
+def test_StationFunctions():
+    # test distance_to() function
+    st1 = Station("Edinburgh Park", "Scotland", "EDP", 55.927615, -3.307829, 0)
+    st2 = Station("Aberdeen", "Scotland", "ABD", 57.143127, -2.097464, 1) 
+    st3 = Station("Station3", "Scotland", "TES", 55.927615, -3.307829, 1) 
+    
+    # distance between station1 and station2 is about 154.19
+    assert round(st1.distance_to(st2), 2) == 154.19
+    # distance between station1 and station3 is 0
+    assert st1.distance_to(st3) == 0
+
+
+def test_RailNetworkFunctions():
+    # test unique crs in the Network
+    st1 = Station("Edinburgh Park", "Scotland", "EDP", 55.927615, -3.307829, 0)
+    st2 = Station("Aberdeen", "Scotland", "EDP", 57.143127, -2.097464, 1)
+    with pytest.raises(IOError) as e:
+        RailNetwork([st1, st2])
+    exec_msg = e.value.args[0]
+    assert exec_msg == f"Input error: two stations with same crs: {st1.crs}."
+
+    # test return information
+
+    # test hub_station() function
+
+    # test closest_hub() function
+
+    # test journey_planner() function
+
+    # test journey_fare() function
+
+    # test plot_fare_to() function
+
